3275488f97b5154d2c645991f64db10b360331b2
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-09-20 12:12:39 +0300

    implement useeffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+
+  const [pages, setPages] = useState([]);
+
+  const getData = async(url) => {
+    const response = await fetch(url);
+    const {pages} = await response.json();
+    setPages(pages);
+  };
+
+  useEffect(() => {
+    getData('./db.json')
+  }, []);
+
+  console.log(pages)
+
   return (
     <div className="home">
       <p>Content</p>
       <div className="content__list">
-        <Link className="textlink" to="/pageone">Lust</Link>
-        <Link className="textlink" to="/pagetwo">Gluttony</Link>
-        <Link className="textlink" to="/pagethree">Greed</Link>
-        <Link className="textlink" to="/pagefour">Sloth</Link>
-        <Link className="textlink" to="/pagefive">Wrath</Link>
-        <Link className="textlink" to="/pagesix">Envy</Link>
-        <Link className="textlink" to="/pageseven">Pride</Link>
+
+        {pages && pages.map((item) => (
+          <Link className="textlink" key={item.id} to={item.link}>{item.title}</Link>
+        ))}
+
       </div>
     </div>
   );
--- src/pages/PageSeven.jsx
+++ src/pages/PageSeven.jsx
@@ -1,51 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { PageBlock } from "../components/PageBlock";
 
 export const PageSeven = () => {
   return (
-    <div>
-      <p>Pride</p>
 
-      <div className="page__text">
-        <Link className="guide__icons" to="/pagesix">
-          <svg
-            fill="#b9b4ad"
-            height="15"
-            width="15"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-          </svg>
-        </Link>
+    <PageBlock/>
 
-        <p className="maintext">
-          Pride (superbia), also known as hubris (from Ancient Greek) or
-          futility. It is considered the original and worst of the seven deadly
-          sins on almost every list, the most demonic. It is also thought to be
-          the source of the other capital sins. Pride is th eopposite of
-          humility. Pride is identified as dangerously corrupt selfishness,
-          putting one's own desires, urges, wants, and whims before the welfare
-          of others. Dante's definition of pride was 'love of self perverted to
-          hatred and contempt for one's neighbor'.
-        </p>
 
-        <Link className="guide__icons" to="/">
-          <svg
-            fill="#b9b4ad"
-            height="15"
-            width="15"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-          </svg>
-        </Link>
-      </div>
+    
+    // <div>
+    //   <p>Pride</p>
 
-      <Link className="textlink" to="/">
-        back to content
-      </Link>
-    </div>
+    //   <div className="page__text">
+    //     <Link className="guide__icons" to="/pagesix">
+    //       <svg
+    //         fill="#b9b4ad"
+    //         height="15"
+    //         width="15"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         viewBox="0 0 384 512"
+    //       >
+    //         <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+    //       </svg>
+    //     </Link>
+
+    //     <p className="maintext">
+    //       Pride (superbia), also known as hubris (from Ancient Greek) or
+    //       futility. It is considered the original and worst of the seven deadly
+    //       sins on almost every list, the most demonic. It is also thought to be
+    //       the source of the other capital sins. Pride is th eopposite of
+    //       humility. Pride is identified as dangerously corrupt selfishness,
+    //       putting one's own desires, urges, wants, and whims before the welfare
+    //       of others. Dante's definition of pride was 'love of self perverted to
+    //       hatred and contempt for one's neighbor'.
+    //     </p>
+
+    //     <Link className="guide__icons" to="/">
+    //       <svg
+    //         fill="#b9b4ad"
+    //         height="15"
+    //         width="15"
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         viewBox="0 0 384 512"
+    //       >
+    //         <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+    //       </svg>
+    //     </Link>
+    //   </div>
+
+    //   <Link className="textlink" to="/">
+    //     back to content
+    //   </Link>
+    // </div>
   );
 };
